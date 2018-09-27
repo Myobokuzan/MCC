46dd2acc098e63b9fd9b5784105beee126ebfd00
Tomás <39780846+Myobokuzan@users.noreply.github.com>
2018-09-27 17:17:57 -0300

    Add files via upload

--- MCC/src/PolinomioInterpolNewthon/Interpolacion.java
+++ MCC/src/PolinomioInterpolNewthon/Interpolacion.java
@@ -5,6 +5,8 @@
  */
 package PolinomioInterpolNewthon;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tomas.quinonez
@@ -16,8 +18,8 @@ public class Interpolacion {
     public static double difDivOrden2(int x0,int x1,int x2){
         return ((difDivOrden1(x1,x2))-difDivOrden1(x0,x1))/(x2-x0);
     }
-    public static double polInterpolacionNewthon(double x0,double x1,double x2){
-        return 3;
+    public static double polInterpolacionNewthon(int x0,int x1,int x2,double x){
+        return f(x0)+(x-x0)*difDivOrden1(x0,x1)+(x-x0)*(x-x1)*difDivOrden2(x0,x1,x2);
     }
     public static double f(int x){
         double res=0;
@@ -34,5 +36,11 @@ public class Interpolacion {
                 res=1;break;
         }
         return res;
+    }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Valores t elegidos: 0, 1, 2");
+        System.out.println("Tiempo t a obtener su velocidad: 1.6");
+        System.out.println("La velocidad del ciclista en el tiempo t="+1.5+" es: "+polInterpolacionNewthon(0,1,2,1.6));
     }
 }
